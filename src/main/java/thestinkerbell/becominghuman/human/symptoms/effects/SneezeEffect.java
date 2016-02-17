eeea05522820564d953257ffc145cf79d38ead47
Janne Beate Bakeng <janne.beate.bakeng@gmail.com>
2016-02-17 21:03:09 +0100

    Added english localization names for potion effects.

--- src/main/java/thestinkerbell/becominghuman/human/symptoms/effects/SneezeEffect.java
+++ src/main/java/thestinkerbell/becominghuman/human/symptoms/effects/SneezeEffect.java
@@ -6,7 +6,7 @@ public final class SneezeEffect extends HumanEffect {
 
 	private static boolean is_bad = true;
 	private static int color = 0;
-	private static String name = "potion.sneezeffect";
+	private static String name = "potion.sneezeeffect";
 	private static ResourceLocation icon = new ResourceLocation("becominghuman", "/textures/potioneffects/sneeze.png".substring(1));
 
 	public SneezeEffect(int id) {
